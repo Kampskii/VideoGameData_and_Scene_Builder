d0288c6342728c065a22834b5434e6b3aeb6ab51
120503tz <zeiglertashon@gmail.com>
2021-04-19 13:59:04 -0400

    added text fields

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,5 +4,13 @@ package sample;
 import javafx.scene.control.TextField;
 
 public class Controller {
+public TextField titleField;
+public TextField devField;
+public TextField pubField;
+public TextField platformField;
+public TextField rankField;
+public TextField yearField;
+public TextField salesField;
+public TextField devCostField;
 
 }
--- src/sample/GameGUI.java
+++ src/sample/GameGUI.java
@@ -12,7 +12,7 @@ public class GameGUI extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         primaryStage.setTitle("Video Game Data & Scene Builder");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 854, 423));
         primaryStage.show();
     }
 
